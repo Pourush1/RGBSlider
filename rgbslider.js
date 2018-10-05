3a758ad3737e8a203582adcb3000f33efdfac0e4
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2018-10-05 12:31:35 -0400

    Developed RGB slider to determine the RGB colors

---
+++ rgbslider.js
@@ -0,0 +1,15 @@
+var slider = document.querySelectorAll("input");
+
+
+	for(var i=0; i<slider.length; i++){
+		slider[i].addEventListener("input", function(){
+			var red = document.getElementById("red").value;
+			var green = document.getElementById("green").value;
+			var blue = document.getElementById("blue").value;
+
+			display.style.background = "rgb(" + red +"," + green + "," + blue + ")";
+
+	});
+	
+}
+
