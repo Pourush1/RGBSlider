ba9d0d504ed00a959f44e7380a247723f0dc1952
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-12-05 02:00:37 -0800

    testing git functionality

--- rgbslider.js
+++ rgbslider.js
@@ -1,15 +1,12 @@
-var slider = document.querySelectorAll("input");
+var slider = document.querySelectorAll('input')
 
+for (var i = 0; i < slider.length; i++) {
+  slider[i].addEventListener('input', function() {
+    var red = document.getElementById('red').value
+    var green = document.getElementById('green').value
+    var blue = document.getElementById('blue').value
 
-	for(var i=0; i<slider.length; i++){
-		slider[i].addEventListener("input", function(){
-			var red = document.getElementById("red").value;
-			var green = document.getElementById("green").value;
-			var blue = document.getElementById("blue").value;
-
-			display.style.background = "rgb(" + red +"," + green + "," + blue + ")";
-
-	});
-	
+    display.style.background = 'rgb(' + red + ',' + green + ',' + blue + ')'
+  })
+  console.log('test')
 }
-
